23cddfe4e45883dd941e3c75538de5c03c608a08
tarikjamil <tarikjamill@gmail.com>
2023-06-20 09:15:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 gsap.registerPlugin(ScrollTrigger);
 
-  // page load starts
-  gsap.to(".main-wrapper", {
+// page load starts
+gsap.to(".main-wrapper", {
     opacity: 1,
     ease: "Quint.easeOut",
     duration: 1
-  });
-  gsap.from(".logo--loading", {
+});
+gsap.from(".logo--loading", {
     opacity: 0,
     ease: "Quint.easeOut",
     duration: 1
-  });
-
+});
 
 // page finish loading
-  window.addEventListener("load", function() {
+window.addEventListener("load", function() {
     gsap.to(".loading-parent", {
         opacity: 0,
         ease: "Quint.easeOut",
-        duration: 0.5
-      });
+        duration: 0.5,
+        delay: 1 // delay in seconds
+    });
     gsap.to(".loading--parent-second", {
         opacity: 1,
         ease: "Quint.easeOut",
-        duration: 0.5
-      });
+        duration: 0.5,
+        delay: 1 // delay in seconds
+    });
     gsap.from("[animation='loading']", {
         opacity: 0,
-        y:"20rem",
+        y: "20rem",
         ease: "Quint.easeOut",
-        duration: 0.5
-      });
+        duration: 0.5,
+        delay: 1 // delay in seconds
+    });
 });
+
   
 
   
