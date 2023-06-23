2f26e24109f426a38178ff1a2c5cc25283e5777e
tarikjamil <tarikjamill@gmail.com>
2023-06-23 11:21:16 +0100

    add smooth scroll back

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
   $(".menu--bg").on("click", function () {
     $(".menu--link").click();
   });
+
+  const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
